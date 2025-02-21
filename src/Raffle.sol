1a89cf36e0a17d140e2f88f9719d73bcff50527b
Michael Eco <michael.gunawan2002@gmail.com>
2025-02-21 09:43:54 +0700

    feat: implement fulfillRandomWords function scaffolding

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -22,7 +22,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
+
 error Raffle__NotEnoughEthToEnterRaffle();
+error Raffle__TransferFailed();
 
 /**
  * @title A smart contract for Raffle
@@ -30,38 +34,81 @@ error Raffle__NotEnoughEthToEnterRaffle();
  * @notice This contract is for creating a sample raffle
  * @dev Implement ChainLink VRFv2.5
  */
-contract Raffle {
-    uint256 private immutable entranceFee;
-    uint256 private immutable interval; // @dev The duration lottery in seconds
-    uint256 private lastTimeStamp;
-    address payable[] private players; // "payable" for paying ETH to the player
+contract Raffle is VRFConsumerBaseV2Plus {
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+    uint256 private immutable i_entranceFee;
+    uint256 private immutable i_interval; // @dev The duration lottery in seconds
+    bytes32 private immutable i_keyHash;
+    uint256 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+    uint256 private s_lastTimeStamp;
+    address payable[] private s_players; // "payable" for paying ETH to the player
+    address private s_recentWinner;
 
     event RaffleEntered(address indexed _player);
 
-    constructor(uint256 _entranceFee, uint256 _interval) {
-        entranceFee = _entranceFee;
-        interval = _interval;
-        lastTimeStamp = block.timestamp;
+    constructor(
+        uint256 _entranceFee,
+        uint256 _interval,
+        address _vrfCoordinator,
+        bytes32 _gasLane,
+        uint256 _subscriptionId,
+        uint32 _callbackGasLimit
+    ) VRFConsumerBaseV2Plus(_vrfCoordinator) {
+        i_entranceFee = _entranceFee;
+        i_interval = _interval;
+        s_lastTimeStamp = block.timestamp;
+        i_keyHash = _gasLane;
+        i_subscriptionId = _subscriptionId;
+        i_callbackGasLimit = _callbackGasLimit;
     }
 
     function enterRaffle() external payable {
-        if (msg.value < entranceFee) {
+        if (msg.value < i_entranceFee) {
             revert Raffle__NotEnoughEthToEnterRaffle();
         }
 
-        players.push(payable(msg.sender));
+        s_players.push(payable(msg.sender));
         emit RaffleEntered(msg.sender);
     }
 
-    // 1. get random number
+    // 1. get random number using ChainLink VRF
     // 2. use random number to pick player
     function pickWinner() external {
-        if (block.timestamp - lastTimeStamp < interval) {
+        if (block.timestamp - s_lastTimeStamp < i_interval) {
             revert();
         }
+
+        uint256 requestId = s_vrfCoordinator.requestRandomWords(
+            VRFV2PlusClient.RandomWordsRequest({
+                keyHash: i_keyHash,
+                subId: i_subscriptionId,
+                requestConfirmations: REQUEST_CONFIRMATIONS,
+                callbackGasLimit: i_callbackGasLimit,
+                numWords: NUM_WORDS,
+                extraArgs: VRFV2PlusClient._argsToBytes(
+                    VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+                )
+            })
+        );
     }
 
     function getEntranceFee() external view returns (uint256) {
-        return entranceFee;
+        return i_entranceFee;
+    }
+
+    // method overriding
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] calldata randomWords
+    ) internal virtual override {
+        uint256 indexOfWinner = randomWords[0] % s_players.length;
+        address payable recentWinner = s_players[indexOfWinner];
+        s_recentWinner = recentWinner;
+        (bool success, ) = recentWinner.call{value: address(this).balance}("");
+        if (!success) {
+            revert Raffle__TransferFailed();
+        }
     }
 }
