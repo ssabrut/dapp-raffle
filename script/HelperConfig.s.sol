1a0cb31a2cda2d3f352f36c120fc59b8877684dc
Michael Eco <michael.gunawan2002@gmail.com>
2025-02-22 04:25:10 +0700

    feat: implement sepolia eth config

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -2,8 +2,16 @@
 pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
+import {VRFCoordinatorV2_5Mock} from "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
 
-contract HelperConfig is Script {
+error HelperConfig__InvalidChainId();
+
+abstract contract Constant {
+    uint256 public constant ETH_SEPOLIA_CHAIN_ID = 11155111;
+    uint256 public constant LOCAL_CHAIN_ID = 31337;
+}
+
+contract HelperConfig is Script, Constant {
     struct NetworkConfig {
         uint256 entranceFee;
         uint256 interval;
@@ -11,5 +19,48 @@ contract HelperConfig is Script {
         bytes32 gasLane;
         uint32 callbackGasLimit;
         uint256 subscriptionId;
+    }
+
+    NetworkConfig public localNetworkConfig;
+    mapping(uint256 => NetworkConfig) public networkConfigs;
+
+    constructor() {
+        networkConfigs[ETH_SEPOLIA_CHAIN_ID] = getSepoliaEthConfig();
+    }
+
+    function getConfigByChainId(
+        uint256 _chainId
+    ) public view returns (NetworkConfig memory) {
+        if (networkConfigs[_chainId].vrfCoordinator != address(0)) {
+            return networkConfigs[_chainId];
+        } else if (_chainId == LOCAL_CHAIN_ID) {
+            return getOrCreateAnvilEthConfig();
+        } else {
+            revert HelperConfig__InvalidChainId();
+        }
+    }
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        return
+            NetworkConfig({
+                entranceFee: 0.01 ether,
+                interval: 30, // 30 seconds
+                vrfCoordinator: 0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B,
+                gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
+                callbackGasLimit: 500000,
+                subscriptionId: 0
+            });
+    }
+
+    function getOrCreateAnvilEthConfig()
+        public
+        view
+        returns (NetworkConfig memory)
+    {
+        if (localNetworkConfig.vrfCoordinator != address(0)) {
+            return localNetworkConfig;
+        }
+
+        // deploy mock contract
     }
 }
