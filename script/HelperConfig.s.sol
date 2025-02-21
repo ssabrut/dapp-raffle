87b35f1dfcc326b1c16e0d3dc398b342fddc0ab7
Michael Eco <michael.gunawan2002@gmail.com>
2025-02-22 04:13:55 +0700

    feat: implement helper config contract

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+
+contract HelperConfig is Script {
+    struct NetworkConfig {
+        uint256 entranceFee;
+        uint256 interval;
+        address vrfCoordinator;
+        bytes32 gasLane;
+        uint32 callbackGasLimit;
+        uint256 subscriptionId;
+    }
+}
