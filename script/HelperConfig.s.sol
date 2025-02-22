5d32e0146016408743d489692244e7c97bca2b3e
Michael Eco <michael.gunawan2002@gmail.com>
2025-02-22 21:23:18 +0700

    feat: implement fund subscription

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
 import {VRFCoordinatorV2_5Mock} from "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
+import {LinkToken} from "test/mocks/LinkToken.sol";
 
 error HelperConfig__InvalidChainId();
 
@@ -74,6 +75,7 @@ contract HelperConfig is Script, Constant {
             MOCK_GAS_PRICE_LINK,
             MOCK_WEI_PER_UNIT_LINK
         );
+        LinkToken linkToken = new LinkToken();
         vm.stopBroadcast();
 
         localNetworkConfig = NetworkConfig({
@@ -82,7 +84,8 @@ contract HelperConfig is Script, Constant {
             vrfCoordinator: address(vrfCoordinatorMock),
             gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
             callbackGasLimit: 500000,
-            subscriptionId: 0
+            subscriptionId: 0,
+            link: address(linkToken)
         });
 
         return localNetworkConfig;
--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.8.19;
 
 import {Script, console2} from "forge-std/Script.sol";
-import {HelperConfig} from "script/HelperConfig.s.sol";
+import {HelperConfig, Constant} from "script/HelperConfig.s.sol";
 import {VRFCoordinatorV2_5Mock} from "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
+import {LinkToken} from "test/mocks/LinkToken.sol";
 
 contract CreateSubscription is Script {
     function createSubscriptionUsingconfig() public returns (uint256, address) {
@@ -34,14 +35,45 @@ contract CreateSubscription is Script {
     }
 }
 
-contract FundSubscription is Script {
+contract FundSubscription is Script, Constant {
     uint256 public constant FUND_AMMOUNT = 3 ether;
 
     function fundSubscriptionUsingconfig() public {
         HelperConfig helperConfig = new HelperConfig();
         address vrfCoordinator = helperConfig.getConfig().vrfCoordinator;
         uint256 subscriptionId = helperConfig.getConfig().subscriptionId;
+        address linkToken = helperConfig.getConfig().link;
+        fundSubscription(vrfCoordinator, subscriptionId, linkToken);
     }
 
-    function run() public {}
+    function fundSubscription(
+        address _vrfCoordinator,
+        uint256 _subscriptionId,
+        address _linkToken
+    ) public {
+        console2.log("Funding subscription: ", _subscriptionId);
+        console2.log("Using VRF Coordinator: ", _vrfCoordinator);
+        console2.log("On ChainId: ", _linkToken);
+
+        if (block.chainid == LOCAL_CHAIN_ID) {
+            vm.startBroadcast();
+            VRFCoordinatorV2_5Mock(_vrfCoordinator).fundSubscription(
+                _subscriptionId,
+                FUND_AMMOUNT
+            );
+            vm.stopBroadcast();
+        } else {
+            vm.startBroadcast();
+            LinkToken(_linkToken).transferAndCall(
+                _vrfCoordinator,
+                FUND_AMMOUNT,
+                abi.encode(_subscriptionId)
+            );
+            vm.stopBroadcast();
+        }
+    }
+
+    function run() public {
+        fundSubscriptionUsingconfig();
+    }
 }
